24870b3419345698f14b26e460792cc4ec707c32
Dmitriy Frolov <omgoole@ya.ru>
2023-09-11 03:25:01 -0700

    small adjustments

--- brain_games/brain_even.py
+++ brain_games/brain_even.py
@@ -7,6 +7,6 @@ GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def generate_game_data(get_random_int=randint) -> tuple[str, str]:
     random_number = get_random_int(0, 100)
-    question = f'Question: {random_number}'
+    question = f'{random_number}'
     answer = 'yes' if is_even(random_number) else 'no'
     return question, answer
--- brain_games/utils.py
+++ brain_games/utils.py
@@ -10,13 +10,15 @@ def run_game(description, generate_data, user_name):
     game_counter = 0
     while game_counter < 3:
         question, answer = generate_data()
-        print(question)
+        print(f'Question: {question}')
         user_input = prompt.string('Your answer: ')
         if user_input == answer:
             print('Correct!')
             game_counter += 1
         else:
-            print(f"'{user_input}' is wrong answer ;(. Correct answer was '{answer}'.")
+            info_one = f"'{user_input}' is wrong answer ;(."
+            info_two = f"Correct answer was '{answer}'."
+            print(f'{info_one} {info_two}')
             print(f"Let's try again, {user_name}!")
             return
     print(f'Congratulations, {user_name}!')
